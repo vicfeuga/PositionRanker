9818f937396f58ba17598aba41911e7bab184928
vicfeuga <50053627+vicfeuga@users.noreply.github.com>
2021-07-26 17:45:30 +0200

    Delete version3 directory

--- version3/final/positionRanker.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from fctpositionRanker import *
-
-print("Entrez l'addresse du workbook (la partie avant /edit dans la barre de recherche):")
-id_workbook = input()
-
-fctpositionRanker(id_workbook)
